1f95030c7d04134d6846a9bee1d7da5870499b4b
Hedrick, Brandon L <bhedrick.purdue@gmail.com>
2018-04-01 23:43:12 -0500

    Add gatsby-image, relevant dependencies and hero image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,57 @@
 import * as React from 'react'
+import styled from 'styled-components'
+
+import Img from "gatsby-image";
 import SplitLayout from '../components/SplitLayout'
 import TitularLayout from '../components/TitularLayout'
+
+interface HeroImage {
+  sizes: object
+}
+interface AboutPageProps {
+  data: {
+    heroImage: HeroImage
+  }
+}
+
+interface DarkColProps {
+  heroImage: HeroImage
+}
 
 const LightCol = () => (
   <TitularLayout title={'About'} subtitle={"I\'m a Javascript developer"}/>
 )
 
-const DarkCol = () => (
+const DarkCol: React.SFC<DarkColProps> = ({ heroImage }) => (
   <div>
+    <HeroImage sizes={heroImage.sizes} />
   </div>
 )
 
-const AboutPage = () => (
+
+const AboutPage: React.SFC<AboutPageProps> = ({ data }) => (
   <SplitLayout
-    light={
-      <LightCol/>
-    }
-    dark={
-      <DarkCol/>
-    }
+  light={
+    <LightCol/>
+  }
+  dark={
+    <DarkCol heroImage={data.heroImage}/>
+  }
   />
 )
 
 export default AboutPage
+
+export const query = graphql`
+  query IndexQuery {
+    heroImage: imageSharp(id: { regex: "/hero/" }) {
+      sizes(maxWidth: 2560 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`;
+
+const HeroImage = styled(Img)`
+  height: 100vh;
+`
