80b1e69c0ffae33c3b6dc90ade86d11fc02f3170
Hedrick, Brandon L <bhedrick.purdue@gmail.com>
2018-04-02 21:53:26 -0500

    ✨ Add new component: MainContentSection

---
+++ src/components/MainContentSection/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import { StyledTypography } from '../../styles/typography';
+
+export const StyledParagraphWrapper = styled.div`
+  margin-top: 1rem;
+  padding-top: 1.5rem;
+  padding-left: 2.5rem;
+  overflow: scroll;
+  ${StyledTypography.paragraph}
+`;
