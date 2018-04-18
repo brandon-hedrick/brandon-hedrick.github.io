8743c19c9fdba0e9c7bd6c2e27da2c6d8dd4c9cb
Hedrick, Brandon L <bhedrick.purdue@gmail.com>
2018-04-18 14:01:18 -0500

    🔧 Fix for regenerator runtime issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,7 @@
  */
 
  // You can delete this file if you're not using it
+ exports.modifyBabelrc = ({ babelrc }) => ({
+    ...babelrc,
+    plugins: babelrc.plugins.concat(['transform-regenerator']),
+  })
